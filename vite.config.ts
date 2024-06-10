250ce885c5c6ea505ab0f563fd4ec95a0e37e353
Herbert Nordson <herbert_nordson@hotmail.com>
2024-06-09 21:12:26 -0300

    feat: build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: "/react-flamme",
   optimizeDeps: {
     include: ['react-slick', 'slick-carousel'] // Inclua quaisquer outras bibliotecas necessárias aqui
   },
